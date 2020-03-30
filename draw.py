b9da56c8dd522556b87bb649ad73c216a28ba07d
Caleb Kahan <ckahan00@stuy.edu>
2020-03-30 06:56:35 -0400

    done

--- draw.py
+++ draw.py
@@ -26,24 +26,24 @@ def add_box( polygons, x, y, z, width, height, depth ):
     z1 = z - depth
 
     #front
-    add_polygon(polygons,x,y,z,x1,y,z,x1,y1,z)
+    add_polygon(polygons,x,y,z,x1,y1,z,x1,y,z)
     add_polygon(polygons,x,y,z,x,y1,z,x1,y1,z)
 
     #back
-    add_polygon(polygons,x,y,z1,x1,y,z1,x1,y1,z1)
-    add_polygon(polygons,x,y,z1,x,y1,z1,x1,y1,z1)
+    add_polygon(polygons,x1,y1,z1,x,y1,z1,x,y,z1)
+    add_polygon(polygons,x1,y1,z1,x,y,z1,x1,y,z1)
 
     #left+Right sides
-    add_polygon(polygons, x, y, z, x, y1, z1,x,y,z1)
-    add_polygon(polygons, x, y, z, x, y1, z,x,y1,z1)
+    add_polygon(polygons, x, y, z, x, y, z1,x,y1,z)
+    add_polygon(polygons, x, y1, z, x, y, z1,x,y1,z1)
     add_polygon(polygons, x1, y, z, x1, y1, z1,x1,y,z1)
     add_polygon(polygons, x1, y, z, x1, y1, z,x1,y1,z1)
 
     #Top+Bottom sides
     add_polygon(polygons,x,y,z,x1,y,z,x,y,z1)
     add_polygon(polygons,x1,y,z,x1,y,z1,x,y,z1)
-    add_polygon(polygons,x,y1,z,x1,y1,z,x,y1,z1)
-    add_polygon(polygons,x1,y1,z,x1,y1,z1,x,y1,z1)
+    add_polygon(polygons,x1,y1,z,x,y1,z,x,y1,z1)
+    add_polygon(polygons,x1,y1,z,x,y1,z1,x1,y1,z1)
 
 
 
